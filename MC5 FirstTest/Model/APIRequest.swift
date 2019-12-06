a93d818c10f171b8076435b9dba3119b6aa1512e
Pedro Henrique Guedes Silveira <pedrohgsilveira@gmail.com>
2019-12-06 14:28:20 -0300

    First Commit

---
+++ MC5 FirstTest/Model/APIRequest.swift
@@ -0,0 +1,9 @@
+//
+//  APIRequest.swift
+//  MC5 FirstTest
+//
+//  Created by Pedro Henrique Guedes Silveira on 06/12/19.
+//  Copyright © 2019 Pedro Henrique Guedes Silveira. All rights reserved.
+//
+
+import Foundation
