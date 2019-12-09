92d868d47079fd2f32941033435d77c1ffcf009e
EdgSgroi <edgar.sgroi@gmail.com>
2019-12-09 18:06:01 -0300

    Server with coredata

---
+++ MC5 FirstTest/Model/ChannelsInfo.swift
@@ -0,0 +1,9 @@
+//
+//  ChannelsInfo.swift
+//  MC5 FirstTest
+//
+//  Created by Edgar Sgroi on 09/12/19.
+//  Copyright © 2019 Pedro Henrique Guedes Silveira. All rights reserved.
+//
+
+import Foundation
